ab6cace8e150a4d88014966a11cd03bdb224c9c6
Oliver Hees <oliverhees@gmail.com>
2023-06-18 13:28:24 +0200

    fix counter error

--- components/ingCount/ingredientCounter.js
+++ components/ingCount/ingredientCounter.js
@@ -1,6 +1,5 @@
-let ingredientCounter = 0;
-
 export function ingCounter(cocktail) {
+  let ingredientCounter = 0;
   const filteredObj = Object.fromEntries(
     Object.entries(cocktail).filter(([key, value]) => {
       if (key.includes("strIngredient") && value != null) {
