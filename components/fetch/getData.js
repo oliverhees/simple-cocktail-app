8577595b5e11dbc1ded3b08e6615c667427147b4
Oliver Hees <oliverhees@gmail.com>
2023-06-18 12:00:15 +0200

    implent no cocktail found message

--- components/fetch/getData.js
+++ components/fetch/getData.js
@@ -1,21 +1,36 @@
 import { createCard } from "../card/card.js";
 
+const msg = document.querySelector(".msg");
+
 //api url
 const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
 export async function getCocktails(search) {
   try {
+    //set search url construct
     const searchUrl = url + search;
     const response = await fetch(searchUrl);
 
+    //chek if response ok 200
     if (response.ok) {
+      //response
       const data = await response.json();
+
+      //log for debug
       console.log(data);
+
+      //set alc default NO
       let alc = "NO";
+
       //check data have drinks
       if (data.drinks === null) {
-        console.log("keine einträge");
+        //display message
+        msg.classList.add("show");
       } else {
+        //hide message
+        msg.classList.remove("show");
+
+        //get drinks
         data.drinks.forEach((cocktail) => {
           //check drink have alcohol
           if (cocktail.strAlcoholic === "Alcohol") {
@@ -23,6 +38,7 @@ export async function getCocktails(search) {
           } else {
             let alc = "NO";
           }
+          //create cocktail card
           createCard(
             cocktail.strCategory,
             cocktail.strDrink,
@@ -35,9 +51,11 @@ export async function getCocktails(search) {
         });
       }
     } else {
+      //log error in console
       console.error("Bad Response");
     }
   } catch (error) {
+    //log erro in console
     console.error(error);
   }
 }
